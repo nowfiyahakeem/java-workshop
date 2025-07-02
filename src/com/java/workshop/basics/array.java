2d9028330c36a76d147357912997f6ee8302dbed
nowfiyahakeem <nowfiyahakeem6@gmail.com>
2025-07-02 14:49:10 +0530

    array java

---
+++ src/com/java/workshop/basics/array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        int[]marks={20,30,40,50,60,70};
+        for(int mark:marks){
+            System.out.println(mark);
+        }
+    }
+}
