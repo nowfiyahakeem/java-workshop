772d471bdd39287d62943022284bd21c607a8a63
nowfiyahakeem <nowfiyahakeem6@gmail.com>
2025-07-02 15:00:37 +0530

    total java

---
+++ src/com/java/workshop/basics/total.java
@@ -0,0 +1,10 @@
+public class total {
+    public static void main(String[] args) {
+        int a=20,b=30,c=40,d=50,e=60;
+
+        int total= a+b+c+d+e;
+
+        System.out.println(total);
+    }
+    
+}
